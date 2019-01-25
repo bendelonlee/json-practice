b11027cab3615370244d565a8a0baa7dc4deec1e
Ben Lee <bendelonlee@gmail.com>
2019-01-25 12:02:30 -0700

    Two failing tests

--- lib/story.rb
+++ lib/story.rb
@@ -1,1 +1,31 @@
 #code goes here
+require "date"
+class Story
+  attr_reader :section, :subsection, :published, :title, :abstract, :link, :photo
+  def initialize(json_hash)
+    json_hash
+    @section = json_hash[:section]
+    @subsection = json_hash[:subsection]
+    @title = json_hash[:title]
+    @abstract = json_hash[:abstract]
+    @link = json_hash[:url]
+    date = json_hash[:created_date]
+    @published = Date.parse(date).strftime("%B %d, %Y") if date
+    @photo = find_normal_photo(json_hash[:multimedia]) || "No Photo Available"
+  end
+
+  def self.load_stories(json_hash)
+    result = []
+    json_hash[:results].each do |object|
+      result << Story.new(object)
+    end
+    result
+  end
+
+  def find_normal_photo(multimedia_hash)
+    photo = multimedia_hash.find { |photo|
+      photo[:format] == "Normal"
+    }
+    photo[:url] if photo
+  end
+end
